55d51f4e71206df4667b705b4a1ef07af98d1148
zGuian <Guianmarcos32@gmail.com>
2025-05-23 06:50:38 -0300

    Ajustado Abstração do RabbitMQ

--- src/shared/SupportHelper.RabbitMQ/RabbitMQConnection.cs
+++ src/shared/SupportHelper.RabbitMQ/RabbitMQConnection.cs
@@ -6,17 +6,24 @@ namespace SupportHelper.RabbitMQ
 {
     public class RabbitMQConnection : IRabbitMQConnection, IDisposable
     {
-        private readonly IConfiguration _configuration;
         private readonly IConnection _connection;
 
         public RabbitMQConnection(IConfiguration configuration)
         {
-            _configuration = configuration;
+            var section = configuration.GetSection("RabbitMQ:Config");
+            var hostname = section["Hostname"];
+            var username = section["Username"];
+            var password = section["Password"];
+            var virtualHost = section["VirtualHost"];
+            var port = section["Port"];
             var factory = new ConnectionFactory
             {
-
+                HostName = hostname ?? throw new ArgumentNullException(null, nameof(hostname)),
+                UserName = username ?? throw new ArgumentNullException(null, nameof(username)),
+                Password = password ?? throw new ArgumentNullException(password, nameof(password)),
+                VirtualHost = virtualHost ?? throw new ArgumentNullException(virtualHost, nameof(virtualHost)),
+                Port = int.Parse(port ?? throw new ArgumentNullException(null, nameof(port))),
             };
-
             _connection = factory.CreateConnectionAsync().GetAwaiter().GetResult();
         }
 
