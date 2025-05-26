bfbb007beda098ad327e89532c327dac883fdc36
zGuian <guianmarcos32@gmail.com>
2025-05-25 21:59:20 -0300

    Alterado propriedades

--- src/shared/SupportHelper.Communication/Requests/MachineInformationRequest.cs
+++ src/shared/SupportHelper.Communication/Requests/MachineInformationRequest.cs
@@ -2,7 +2,15 @@
 {
     public record MachineInformationRequest
     {
-        public required string Hostname { get; init; }
+        public string Hostname { get; init; }
         public string? Ipv4 { get; init; }
+        public string? ReplyToQueueName { get; init; }
+
+        public MachineInformationRequest(string hostname, string? ipv4, string? replyToQueueName)
+        {
+            Hostname = hostname;
+            Ipv4 = ipv4;
+            ReplyToQueueName = replyToQueueName;
+        }
     }
 }
--- src/shared/SupportHelper.Communication/Responses/MachineInformationResponse.cs
+++ src/shared/SupportHelper.Communication/Responses/MachineInformationResponse.cs
@@ -3,7 +3,28 @@
     public record MachineInformationResponse
     {
         public string? IdServiceWindows { get; init; }
-        public required string Hostname { get; init; }
-        public string? Ipv4 { get; init; }
+        public string Hostname { get; init; }
+        public NetworkBoad[]? NetworkBoads { get; init; }
+
+        public MachineInformationResponse(string hostname, string? idServiceWindows, NetworkBoad[]? networkBoads)
+        {
+            IdServiceWindows = idServiceWindows;
+            Hostname = hostname;
+            NetworkBoads = networkBoads;
+        }
+    }
+
+    public record NetworkBoad
+    {
+        public string? Ipv4 { get; private set; }
+        public string? Ipv6 { get; private set; }
+        public string? MacAddress { get; private set; }
+
+        public NetworkBoad(string? ipv4, string? ipv6, string? macAddress)
+        {
+            Ipv4 = ipv4;
+            Ipv6 = ipv6;
+            MacAddress = macAddress;
+        }
     }
 }
