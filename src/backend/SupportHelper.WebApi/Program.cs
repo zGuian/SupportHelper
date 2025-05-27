caafa93c85e3ebcaf5d01efdbe7a7d84db1e81b3
zGuian <Guianmarcos32@gmail.com>
2025-05-27 14:12:58 -0300

    Injetado Abstração de RabbitMQ.

--- src/backend/SupportHelper.WebApi/Program.cs
+++ src/backend/SupportHelper.WebApi/Program.cs
@@ -1,7 +1,11 @@
+using SupportHelper.RabbitMQ.Implementation;
+using SupportHelper.RabbitMQ.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddSingleton<IRabbitMQConnection>(
+    await RabbitMQConnection.CreateConnectionToRabbitMQ(builder.Configuration));
 builder.Services.AddControllers();
 
 var app = builder.Build();
