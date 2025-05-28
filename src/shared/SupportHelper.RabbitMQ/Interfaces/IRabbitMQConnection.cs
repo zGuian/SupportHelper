4520b28c049399968ee9f78a7a29ad264bc284d6
zGuian <Guianmarcos32@gmail.com>
2025-05-28 14:13:29 -0300

    Alterado interface de connection.

--- src/shared/SupportHelper.RabbitMQ/Interfaces/IRabbitMQConnection.cs
+++ src/shared/SupportHelper.RabbitMQ/Interfaces/IRabbitMQConnection.cs
@@ -5,5 +5,7 @@ namespace SupportHelper.RabbitMQ.Interfaces
     public interface IRabbitMQConnection
     {
         IConnection Connection { get; }
+
+        Task<IChannel> CreateQueueInExchange(string hostname, CancellationToken  cancellationToken = default);
     }
 }
