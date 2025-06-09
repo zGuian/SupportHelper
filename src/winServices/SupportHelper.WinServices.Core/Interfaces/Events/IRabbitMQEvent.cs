7430ca1278857051cb36464fb176c4f19b20ddd5
zGuian <Guianmarcos32@gmail.com>
2025-06-09 09:45:55 -0300

    [Feat]: Criado interface para maior controle de codigo.

---
+++ src/winServices/SupportHelper.WinServices.Core/Interfaces/Events/IRabbitMQEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace SupportHelper.WinServices.Core.Interfaces.Events
+{
+    public interface IRabbitMQEvent
+    {
+        Task ListenRabbitQueueDefault(IConfiguration configuration, CancellationToken cancellationToken);
+    }
+}
