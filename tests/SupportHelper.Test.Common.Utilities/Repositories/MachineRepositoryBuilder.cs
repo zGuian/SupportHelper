097783c28152307fb9e3daac3a2dd78cbe18cd26
zGuian <Guianmarcos32@gmail.com>
2025-06-11 08:55:01 -0300

    [Test]: Iniciado projetos de testes unitarios.

---
+++ tests/SupportHelper.Test.Common.Utilities/Repositories/MachineRepositoryBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Moq;
+using SupportHelper.Domain.Interfaces.Repositories;
+
+namespace SupportHelper.Test.Common.Utilities.Repositories
+{
+    public class MachineRepositoryBuilder
+    {
+        public static IMachineRepository Build()
+        {
+            var mock = new Mock<IMachineRepository>();
+            return mock.Object;
+        }
+    }
+}
--- tests/SupportHelper.Tests/ControllersTest/MachineControllerTest.cs
+++ tests/SupportHelper.Tests/ControllersTest/MachineControllerTest.cs
@@ -1,30 +1,10 @@
-﻿using Bogus;
-using Moq;
-using SupportHelper.Communication.Requests;
-using SupportHelper.Communication.Responses;
-using SupportHelper.WebApi.Controllers;
-
-namespace SupportHelper.Tests.ControllersTest
+﻿namespace SupportHelper.Tests.ControllersTest
 {
     public class MachineControllerTest
     {
-        private readonly Mock<MachineController> _machineControllerMock;
-
-        public MachineControllerTest()
-        {
-            _machineControllerMock = new Mock<MachineController>();
-        }
-
         [Fact]
-        public async Task GetMachineInformation_ShouldBeMachine()
+        public void GetMachineInformation_ShouldBeMachine()
         {
-            var faker = new Faker("pt-BR");
-            var hostname = faker.Random.String();
-            var ipv4 = faker.Internet.Ip();
-            var replyToQueueName = faker.Random.String();
-            var rabbit = new RabbitMQRequest(hostname, ipv4, replyToQueueName);
-            var request = new MachineInformationRequest("", rabbit);
-            var response = new Mock<MachineInformationResponse>(hostname, Guid.NewGuid().ToString());
         }
     }
 }
