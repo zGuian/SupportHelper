df470097213b53427e7c902ef9e7fc74e8df1d28
zGuian <Guianmarcos32@gmail.com>
2025-06-12 09:18:16 -0300

    [Fix]: Adicionado Dependencia ao IOC .

--- src/winServices/SupportHelper.WinServices.Core/DependencyInjection.cs
+++ src/winServices/SupportHelper.WinServices.Core/DependencyInjection.cs
@@ -2,8 +2,10 @@
 using SupportHelper.WinServices.Core.Interfaces;
 using SupportHelper.WinServices.Core.Interfaces.Events;
 using SupportHelper.WinServices.Core.Interfaces.RabbitMQService;
+using SupportHelper.WinServices.Core.Interfaces.UseCases;
 using SupportHelper.WinServices.Core.Services;
 using SupportHelper.WinServices.Core.Services.RabbitMQServices;
+using SupportHelper.WinServices.Core.UseCases;
 using SupportHelper.WinServices.Core.Workers;
 
 namespace SupportHelper.WinServices.Core
@@ -18,6 +20,7 @@ namespace SupportHelper.WinServices.Core
             services.AddHostedService<RabbitEventWorker>();
 
             services.AddTransient<IMachineService, MachineService>();
+            services.AddTransient<IGetLoggerSgpClientUseCase, GetLoggerSgpClientUseCase>();
             return services;
         }
     }
