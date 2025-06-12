f18b6041b8b3e940a5825f7820bd2a846dd36625
zGuian <Guianmarcos32@gmail.com>
2025-06-12 09:17:54 -0300

    [Refact]: Refatorado para maior entendimento.

--- src/backend/SupportHelper.Infrastructure/MQServices/MachineMQService.cs
+++ src/backend/SupportHelper.Infrastructure/MQServices/MachineMQService.cs
@@ -8,7 +8,7 @@ using System.Text.Json;
 
 namespace SupportHelper.Infrastructure.MQServices
 {
-    public class MachineMQService : IMachineMQServices
+    public sealed class MachineMQService : IMachineMQServices
     {
         private readonly ILogger<MachineMQService> _logger;
         private readonly IRabbitMQConnection _connection;
@@ -19,7 +19,42 @@ namespace SupportHelper.Infrastructure.MQServices
             _logger = logger;
         }
 
-        public async Task PublishGetInformationAsync(MachineInformationRequest request)
+        public async Task PublishMessageAsync(RequestBase<RequestMachine> request, RabbitMQRequest rabbitMQRequest)
+        {
+            try
+            {
+                var channel = await _connection.DeclareExchangeAndQueueDefaultAsync();
+                var correlationId = Guid.NewGuid().ToString();
+
+                var jsonString = JsonSerializer.Serialize(request);
+                var body = Encoding.UTF8.GetBytes(jsonString);
+                var properties = new BasicProperties
+                {
+                    CorrelationId = correlationId,
+                    ContentType = "application/json",
+                    ContentEncoding = "UTF8",
+                    Timestamp = new AmqpTimestamp(),
+                    ReplyTo = "queue.response.machines"
+                };
+
+                var routingKey = $"worker.machine.{rabbitMQRequest.Hostname}";
+
+                await channel.BasicPublishAsync(exchange: _connection.ConfigurationValue["exchange"],
+                                                routingKey: routingKey,
+                                                mandatory: true,
+                                                basicProperties: properties,
+                                                body: body);
+
+                _logger.LogInformation("publicado mensagem");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("Erro ao publicar mensagem. Error {message}", ex.Message);
+                throw;
+            }
+        }
+
+        public async Task PublishMessageAsync(MachineInformationRequest request)
         {
             try
             {
--- src/winServices/SupportHelper.WinServices.Core/Interfaces/IMachineService.cs
+++ src/winServices/SupportHelper.WinServices.Core/Interfaces/IMachineService.cs
@@ -1,9 +1,11 @@
 ﻿using SupportHelper.WinServices.Core.Models;
+using SupportHelper.WinServices.Core.Models.Enums;
 
 namespace SupportHelper.WinServices.Core.Interfaces
 {
     public interface IMachineService
     {
         MachineModel GetInformationMachine(CancellationToken cancellationToken = default);
+        void MakeAvailableLogSgpClient(SGPClientLine productionLine);
     }
 }
