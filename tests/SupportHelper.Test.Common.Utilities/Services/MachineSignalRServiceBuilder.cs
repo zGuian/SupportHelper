1fbb2010ccb61694703b500e56c5614be15f4986
zGuian <Guianmarcos32@gmail.com>
2025-06-17 14:16:09 -0300

    [TEST]: Adicionado Mock

---
+++ tests/SupportHelper.Test.Common.Utilities/Services/MachineSignalRServiceBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Moq;
+using SupportHelper.Domain.Interfaces.MQServices;
+using SupportHelper.Domain.Interfaces.SignalRContext;
+
+namespace SupportHelper.Test.Common.Utilities.Services
+{
+    public class MachineSignalRServiceBuilder
+    {
+        public static IMachineSignalRServices Build()
+        {
+            var mock = new Mock<IMachineSignalRServices>();
+            return mock.Object;
+        }
+    }
+}
